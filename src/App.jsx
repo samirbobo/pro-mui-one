a69939e526884fcac732ffd496144b56ff393ca4
samir <samir.elanany@microengineering.ca>
2024-06-29 21:20:37 +0300

    init-project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="create" element={<Create />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
