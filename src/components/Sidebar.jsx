1a272b9c14aa31dad2d53f80898a7f0fbc1e2aca
samir <samir.elanany@microengineering.ca>
2024-07-13 20:53:40 +0300

    Finished Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ import CreateIcon from "@mui/icons-material/Create";
 import PersonIcon from "@mui/icons-material/Person";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 
 /*
@@ -38,10 +38,24 @@ import { Brightness4, Brightness7 } from "@mui/icons-material";
   2- Link from material ui href="/"
   3- Link from react-router 
   4- useNavigate from react-router 
+
+  Breakpoints: زي الميديا كويري بالظبط في ال سي اس اس 
+  xs, extra-small: From 0px to 600px
+  sm, small: From 600px to 900px 
+  md, medium: From 900px to 1200px
+  lg, large: From 1200px to 1536px
+  xl, extra-large: From 1536px to larger than
 */
-export default function Sidebar({ drawerWidth, setChangeMode }) {
+
+export default function Sidebar({
+  drawerWidth,
+  setChangeMode,
+  showSidebar,
+  setShowSidebar,
+}) {
   const navigate = useNavigate();
   const theme = useTheme(); // هوك صمم من ماتريل يو اي عشان اقدر اوصل لتفاصيل الموقع الحالي من مود هل لايت ولا ضارك الالوان وهكذا
+  const currentLocation = useLocation();
 
   const links = [
     { label: "Home", href: "/" },
@@ -60,6 +74,7 @@ export default function Sidebar({ drawerWidth, setChangeMode }) {
   return (
     <Drawer
       sx={{
+        display: { xs: showSidebar ? "block" : "none", sm: "block" }, // دي الطريقه الاولي لجعل العناصر رسبونسف علي كل الشاشات
         width: `${drawerWidth}px`,
         flexShrink: 0,
         "& .MuiDrawer-paper": {
@@ -67,7 +82,9 @@ export default function Sidebar({ drawerWidth, setChangeMode }) {
           boxSizing: "border-box",
         },
       }}
-      variant="permanent"
+      open={showSidebar}
+      variant={showSidebar ? "temporary" : "permanent"}
+      onClose={() => setShowSidebar(false)}
       anchor="left"
     >
       <List>
@@ -75,7 +92,7 @@ export default function Sidebar({ drawerWidth, setChangeMode }) {
           sx={{ display: "flex", justifyContent: "center", mb: "12px" }}
           disablePadding
         >
-          <IconButton onClick={changeAndSaveMode} divider>
+          <IconButton onClick={changeAndSaveMode}>
             {theme.palette.mode === "dark" ? (
               <Brightness7 sx={{ fontSize: "1.7rem", color: "orange" }} />
             ) : (
@@ -84,8 +101,17 @@ export default function Sidebar({ drawerWidth, setChangeMode }) {
           </IconButton>
         </ListItem>
         <Divider />
+
         {links.map(({ label, href }) => (
-          <ListItem key={label} disablePadding>
+          <ListItem
+            sx={{
+              bgcolor:
+                currentLocation.pathname === href &&
+                theme.palette.favColor.main,
+            }}
+            key={label}
+            disablePadding
+          >
             <ListItemButton
               onClick={() => {
                 navigate(href);
